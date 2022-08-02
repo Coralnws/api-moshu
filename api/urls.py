f994932fab21377b596934cbdf4a513b306003df
KaiBa Fou <foueducation@gmail.com>
2022-08-02 10:00:54 +0700

    CRUD in project (not yet include documents)

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from api.views.userApiView import *
+from api.views.projectApiView import *
 
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import (
@@ -15,6 +16,13 @@ urlpatterns = [
     path('auth/register/', UserRegisterView.as_view(), name="register"),
     path('auth/login/', LoginView.as_view(), name="login"),
     path('auth/logout/', LogoutView.as_view(), name="logout"),
+
+
+    # Project
+    path('project/<uuid:projectId>', ProjectDetailView.as_view(), name="project_detail"),
+    path('project/create/<uuid:teamId>', ProjectCreateView.as_view(), name="create_project"),
+    path('project/list', ProjectListView.as_view(), name="project_list"),
+    
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
---
+++ api/views/projectApiView.py
@@ -0,0 +1,131 @@
+import operator
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework import generics, status, permissions
+from drf_yasg.utils import swagger_auto_schema
+
+from ..serializers.projectSerializers import *
+from ..models.projects import *
+from ..models.userRelations import UserTeam
+from ..api_throttles import *
+
+
+""" For Admin(superuser)
+GET: Get Project Detail By Id  # for any
+PUT: Update Project By Id      # for superuser or owner
+DELETE: Delete Project By Id (set isDelete = True)     # for superuser or owner
+"""
+class ProjectDetailView(generics.GenericAPIView):
+    serializer_class = ProjectDetailSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+    throttle_classes = [anonRelaxed, userRelaxed]
+
+    def get_serializer_class(self):
+        if self.request.method in ['GET']:
+            return ProjectProfileSerializer
+        return ProjectDetailSerializer
+
+    # Get Project Detail By Id
+    @swagger_auto_schema(operation_summary="Get Project Detail By Id")
+    def get(self, request, projectId):
+        # try:
+            project = get_object_or_404(Project, pk=projectId)
+            
+            serializer = self.get_serializer(instance=project)
+            data = serializer.data
+            data['message'] = "Get Project Detail Successfully"
+            return Response(data, status=status.HTTP_200_OK)
+        # except:
+        #     return Response({"message": "Get Project Detail Failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Update Project By Id
+    @swagger_auto_schema(operation_summary="Update Project By Id")
+    def put(self, request, projectId):
+        # try:
+            project = get_object_or_404(Project, pk=projectId)
+            # will open permission for when the person is the mainAdmin or admin of the team
+            if not request.user.is_staff and request.user != project.createdBy:
+                return Response({"message": "Unauthorized for update feed"}, status=status.HTTP_401_UNAUTHORIZED)
+            serializer = self.get_serializer(instance=project, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(updatedAt=timezone.now())
+            data = serializer.data
+            data['message'] = "Update Project Successfully"
+            return Response(data, status=status.HTTP_200_OK)
+        # except:
+        #     return Response({"message": "Update Project Failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # Delete Project By Id
+    @swagger_auto_schema(operation_summary="Delete Project By Id")
+    def delete(self, request, projectId):
+        # try:
+            project = get_object_or_404(Project, pk=projectId)
+            if request.user == project.createdBy or request.user.is_staff:
+                project.delete()
+                return Response({"message": "Delete Project Successfully"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Unauthorized for Delete Project"}, status=status.HTTP_401_UNAUTHORIZED)
+        # except:
+        #     return Response({"message": "Delete Project Failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+POST: Create Project
+"""
+class ProjectCreateView(generics.CreateAPIView):
+    serializer_class = ProjectCreateSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    throttle_classes = [anonRelaxed, userRelaxed]
+
+    @swagger_auto_schema(operation_summary="Create Project")
+    def post(self, request, teamId):
+        # try:
+            isMember = UserTeam.objects.get(team=teamId, user=request.user)
+            # print("isMember", isMember)
+            if isMember:
+                if not isMember.isAdmin and not isMember.isMainAdmin:
+                    return Response({"message": "Unauthorized to Create Project in Team"}, status=status.HTTP_403_FORBIDDEN)
+            team = get_object_or_404(Team, pk=teamId)
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(createdBy=request.user, belongTo=team)
+            data = serializer.data
+            data['message'] = "Create Project Successfully"
+            return Response(data, status=status.HTTP_201_CREATED)
+        # except:
+        #     return Response({"message": "Create Project Failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+GET: Get All Projects
+"""
+class ProjectListView(generics.ListAPIView):
+    serializer_class = ListProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    throttle_classes = [anonRelaxed, userRelaxed]
+
+    # Get All Projects
+    @swagger_auto_schema(operation_summary="Get All Projects")
+    def get_queryset(self):
+        orderBy = self.request.GET.get('orderBy')
+        search = self.request.GET.get('search')
+        group = self.request.GET.get('belongTo')
+        createdBy = self.request.GET.get('createdBy')
+
+        filter = Q()
+        if search is not None:
+            searchTerms = search.split(' ')
+            for term in searchTerms:
+                filter &= Q(title__icontains=term) | Q(description__icontains=term) | Q(createdBy__username__icontains=term)
+
+        if group is not None:
+            filter &= Q(belongTo=group)
+
+        if createdBy is not None:
+            filter &= Q(createdBy = createdBy)
+
+        allProjects = Project.objects.filter(filter).order_by('-createdAt')
+        return allProjects; # will implement ordered By soon
