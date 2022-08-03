48af4756cd99f109fe162a18c28066a6e19acc4f
Ubuntu <ubuntu@ip-172-31-33-179.ap-northeast-2.compute.internal>
2022-08-03 10:42:53 +0000

    [config]: update .ini and settings for stagging

--- moshu/settings.py
+++ moshu/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stagging-api-moshu.cheangfou.com', 'www.stagging-api-moshu.cheangfou.com', 'uwsgimoshu']
 
 
 # Application definition
