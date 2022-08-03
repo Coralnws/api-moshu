75df14b8d75f9869a16892fddbf20bfa38a3c7aa
Ubuntu <ubuntu@ip-172-31-33-179.ap-northeast-2.compute.internal>
2022-08-03 09:46:41 +0000

    [config]: for stagging

--- moshu/settings.py
+++ moshu/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 from datetime import timedelta  # for auth token lifetimes
 
@@ -221,6 +222,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
